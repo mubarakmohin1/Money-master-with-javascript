a3a7451d11e1a4809e57790c39bcc30b3e7311e8
mubarak <chemistmubarak1@gmail.com>
2022-02-17 19:46:36 +0600

    first commit

--- money.js
+++ money.js
@@ -1,7 +1,12 @@
+//  compulsory part////
+/////////////////////////////
 
 function getInputvalue(inputId){
     const firstIncome = document.getElementById(inputId);
     const firstTotal = firstIncome.value;
+       
+    //   error handling for all positive number/////
+
     if(firstTotal > 0){
         const firstValue = parseFloat(firstTotal);
         // firstIncome.value = "";
@@ -13,7 +18,8 @@ function getInputvalue(inputId){
 } 
  
 document.getElementById('calculate-btn').addEventListener('click',function(){
-        
+        // function call////
+
         const firstValue = getInputvalue('income-total');
        
  
@@ -27,11 +33,7 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
     const totalExpenses = foodExpenseAmount + rentExpenseAmount + clotheExpenseAmount; 
     const totalBalance = firstValue - totalExpenses;
        
-        
-     
-     
-   
-    // const firstValue = getInputvalue('income-total');
+     // const firstValue = getInputvalue('income-total');
   
     
      const finalExpense = document.getElementById('total-expense');
@@ -40,6 +42,10 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
      const firstBalance = document.getElementById('last-balance');
      firstBalance.innerText = totalBalance;
     })
+
+
+    // bonus part////
+    //////////////////
 
 
     function getInputcalculation(calcInput){
@@ -74,27 +80,27 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
         const firstBalance = document.getElementById('last-balance');
         firstBalance.innerText = balanceTotal;
 
-
-
-
-       
-        const savePercentValue = getInputcalculation('save-parcent');
+         const savePercentValue = getInputcalculation('save-parcent');
 
         const saveAmount = document.getElementById('save-amount') ;
         // const saveAmount = getInputcalculation('save-amount');
+
+
+        // error handling //////
+        ///////////////////////////
+
         if(savePercentValue > 0 && savePercentValue <= 100){
             const successMsg = document.getElementById('verify-success');
             successMsg.style.display = 'block';
+            saveAmount.innerText =  valueFirst * (savePercentValue/100);
+
+            const remainAmount = document.getElementById('remain-balance');
+            remainAmount.innerText = balanceTotal - saveAmount.innerText;
         }
         else{
             const errorMsg = document.getElementById('verify-fail');
             errorMsg.style.display = 'block';
         }
-        
-       saveAmount.innerText =  valueFirst * (savePercentValue/100);
-
-        const remainAmount = document.getElementById('remain-balance');
-        remainAmount.innerText = balanceTotal - saveAmount.innerText;
         
           
     })
